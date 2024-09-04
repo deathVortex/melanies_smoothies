a137d7b230a2a2cc89a82df5e5faed660abe32f8
Yassine El Ghoul <51198357+deathVortex@users.noreply.github.com>
2024-09-04 11:40:30 +0100

    Update streamlit_app.py
    New section to display fruityvice nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("""Choose the fruits you want in your custom smoothie!""")
 
 name_on_order = st.text_input("Name on Smoothie:")
-st.write("the name on your smoothie will be:", name_on_order)
+st.write("The name on your smoothie will be:", name_on_order)
 
 #session = get_active_session()
 cnx = st.connection("snowflake")
@@ -42,3 +42,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+# New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
